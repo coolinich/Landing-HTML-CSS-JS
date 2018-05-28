555ab1f47532d51c8892afaaee00ec97c381a5e0
coolinich <coolinich@gmail.com>
2018-05-29 01:59:48 +0300

    started responsive, added slicknav, ask question to Denis

--- www/js/init.js
+++ www/js/init.js
@@ -2,7 +2,13 @@ $( function() {
     // init tabs
     $( "#services-tabs" ).tabs();
     $( "#tabs-portfolio" ).tabs();
+    //init carousel
     $('.reviews-carousel').slick({
         dots: true
+    });
+    //init slicknav (responsive menu)
+    $('.header-nav').slicknav({
+        appendTo: '#header .container-fluid',
+        label: ''
     });
 } );
