2cfed892516ca51185daa6fca70e8684d1050c6e
coolinich <coolinich@gmail.com>
2018-05-29 23:46:03 +0300

    fixed menu, small fixes in about us and portfolio

---
+++ www/js/header-on-scroll.js
@@ -0,0 +1,44 @@
+const header = $('.header');
+const windowHeight = $(window).height();
+const headerHeight = header.outerHeight();
+
+function onScroll(e) {
+    let pos = $(window).scrollTop();
+
+    if (pos > headerHeight + 100) {
+        header.css({
+            'position': 'fixed',
+            'top': '-75px',
+            'background-color': '#000'
+        });
+    }
+    if (pos > windowHeight) {
+        header.css({
+            'top': '0',
+            'transition': 'top .3s ease-out'
+        });
+    }
+
+    if (pos < headerHeight + 100) {
+        header.css({
+            'position': 'absolute',
+            'top': '0',
+            'background-color': 'transparent',
+            'transition': 'none'
+        });
+    }
+}
+
+$(window).on('scroll', onScroll);
+
+// Scroll to element
+const scrollBtn = $('[data-scroll]');
+
+function onClick(e) {
+    e.preventDefault();
+    const target = $(this).attr('data-scroll');
+    const dist = $(target).offset().top;
+    $('html, body').animate({ scrollTop: dist }, 1000, 'swing');
+}
+
+scrollBtn.on('click', onClick);
