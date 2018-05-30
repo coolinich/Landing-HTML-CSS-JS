f53ad3779f76be064390d43fe2634339894ba02a
Olha Kulinich <o.kulinich@levi9.com>
2018-05-31 00:23:34 +0300

    added buttons in carousel

--- www/js/header-on-scroll.js
+++ www/js/header-on-scroll.js
@@ -1,25 +1,25 @@
-const header = $('.header');
-const windowHeight = $(window).height();
-const headerHeight = header.outerHeight();
+const header = $('#header'); // находим элемент
+const windowHeight = $(window).height(); // узнаем высоту экран
+const headerHeight = header.outerHeight(); // узнаем высоту шапки
 
 function onScroll(e) {
-    let pos = $(window).scrollTop();
+    let pos = $(window).scrollTop(); // определяем позицию скрола 
 
-    if (pos > headerHeight + 100) {
+    if (pos > headerHeight + 100) { // если проскролели больше высоты шапки +100пикс
         header.css({
             'position': 'fixed',
-            'top': '-75px',
+            'top': -(headerHeight + 50),
             'background-color': '#000'
         });
     }
-    if (pos > windowHeight) {
+    if (pos > windowHeight) { // если проскролели больше высоты экрана
         header.css({
             'top': '0',
             'transition': 'top .3s ease-out'
         });
     }
 
-    if (pos < headerHeight + 100) {
+    if (pos < headerHeight + 100) { // если проскролели меньше высоты шапки
         header.css({
             'position': 'absolute',
             'top': '0',
@@ -29,16 +29,16 @@ function onScroll(e) {
     }
 }
 
-$(window).on('scroll', onScroll);
+$(window).on('scroll', onScroll); // вешаем событие на скролл в окне
 
 // Scroll to element
-const scrollBtn = $('[data-scroll]');
+const scrollBtn = $('[data-scroll]'); // ищем все элементы у которого есмть атрибут data-scroll
 
 function onClick(e) {
-    e.preventDefault();
-    const target = $(this).attr('data-scroll');
-    const dist = $(target).offset().top;
-    $('html, body').animate({ scrollTop: dist }, 1000, 'swing');
+    e.preventDefault(); // отменить стандартное действие
+    const target = $(this).attr('data-scroll'); // получаем значение атрибута data-scroll 
+    const dist = $(target).offset().top; // меряем растояние до блока
+    $('html, body').animate({ scrollTop: dist }, 1000, 'swing'); // анимируем скролл до нужного блока 
 }
 
-scrollBtn.on('click', onClick);
+scrollBtn.on('click', onClick); // отслеживаем событие клик на все элементы с атрр data-scroll
--- www/js/init.js
+++ www/js/init.js
@@ -4,7 +4,11 @@ $( function() {
     $( "#tabs-portfolio" ).tabs();
     //init carousel
     $('.reviews-carousel').slick({
-        dots: true
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        prevArrow: '<div class="slick-prev slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-left"></i></div></div>',
+        nextArrow: '<div class="slick-next slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-right"></i></div></div>'
     });
     //init slicknav (responsive menu)
     $('.header-nav').slicknav({
